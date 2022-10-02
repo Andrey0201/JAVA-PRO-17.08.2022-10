176f2afd17c200f2bf1ae779b25ccd871cfd0958
Andrii <andriutsa1@ukr.net>
2022-10-02 19:44:16 +0200

    Fixed: modifications & extract method getThreads(&edit place start,join)

--- src/com/javapro/lesson12/model/ValueCalculator.java
+++ src/com/javapro/lesson12/model/ValueCalculator.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class ValueCalculator {
 
-  float[] array;
-  int size = 500000;
-  int halfSize = size / 2;
+  private float[] array;
+  private final int size = 500000;
+  private final int halfSize = size / 2;
 
   public void doCalc() {
     long start = System.currentTimeMillis();
@@ -14,30 +14,33 @@ public class ValueCalculator {
     array = new float[size];
     float[] array2 = new float[halfSize];
     float[] array3 = new float[halfSize];
-
     Arrays.fill(array, 5);
 
     System.arraycopy(array, halfSize, array2, 0, halfSize);
     System.arraycopy(array, halfSize, array3, 0, halfSize);
 
-    Thread thread = new MyThread(array2);
-    Thread thread1 = new MyThread(array3);
-
-    try {
-      thread.start();
-      thread.join();
-      thread1.start();
-      thread1.join();
-    } catch (RuntimeException | InterruptedException e) {
-      System.out.println("exception from thread");
-
-    }
+    getThreads(array2, array3);
 
     System.arraycopy(array2, 0, array, 0, halfSize);
     System.arraycopy(array3, 0, array, halfSize, halfSize);
     System.out.println(Thread.currentThread().getName() + " finish");
     long finish = System.currentTimeMillis() - start;
     System.out.println("runtime:" + finish + " ms");
+  }
+
+  private static void getThreads(float[] array2, float[] array3) {
+    Thread thread = new MyThread(array2);
+    Thread thread1 = new MyThread(array3);
+
+    try {
+      thread.start();
+      thread1.start();
+      thread.join();
+      thread1.join();
+    } catch (RuntimeException | InterruptedException e) {
+      System.out.println("exception from thread");
+
+    }
   }
 }
 
